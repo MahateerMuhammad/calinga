df1dca2113ef59674acaf7ddb23a7ef920ef3538
igmoiiz <khanmoaiz682@gmail.com>
2025-08-08 00:39:40 +0500

    Increased the ndk version support - Moiz Baloch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 android {
     namespace = "com.example.calinga"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "29.0.13599879"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
